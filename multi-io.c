4e1ea5e9593085ca1a7b89161dbf30b0ed0e899d
wangzhixian <386323419@qq.com>
2026-07-02 01:07:27 +0800

    epoll

--- multi-io.c
+++ multi-io.c
@@ -6,6 +6,8 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <sys/select.h>
+#include <poll.h>
+#include <sys/epoll.h>
 
 // block
 void *client_thread(void *arg) {
@@ -69,7 +71,7 @@ int main() {
         pthread_t thid;
         pthread_create(&thid, NULL, client_thread, &clientfd);
     }
-#else // select
+#elif 0 // select
     // int nready = select(maxfd, rset, wset, eset, timeout);
 
     fd_set rfds, rset;
@@ -107,6 +109,85 @@ int main() {
             }
         }
     }
+#elif 0 // poll
+    struct pollfd fds[1024] = {0};
+    fds[sockfd].fd = sockfd;
+    fds[sockfd].events = POLLIN;
+    int maxfd = sockfd;
+
+    while (1) {
+        int nready = poll(fds, maxfd + 1, -1);
+
+        if (fds[sockfd].revents & POLLIN) {
+            struct sockaddr_in clientaddr;
+            socklen_t len = sizeof(clientaddr);
+            int clientfd = accept(sockfd, (struct sockaddr*)&clientaddr, &len);
+            
+            printf("socket: %d\n", clientfd);
+
+            fds[clientfd].fd = clientfd;
+            fds[clientfd].events = POLLIN;
+            maxfd = clientfd;
+        }
+
+        int i = 0;
+        for (i = sockfd + 1; i <= maxfd; i++) {
+            if (fds[i].revents & POLLIN) {
+                char buffer[128] = {0};
+                int count = recv(i, buffer, sizeof(buffer), 0);
+                if (count == 0) {
+                    printf("disconnect\n");
+                    fds[i].fd = -1;
+                    fds[i].events = 0;
+                    close(i);
+                    continue;
+                }
+                send(i, buffer, count, 0);
+                printf("clientfd: %d, count: %d, buffer: %s\n", i, count, buffer);
+            }
+        }
+    }
+#else // epoll
+    int epfd = epoll_create(1);
+
+    struct epoll_event ev;
+    ev.events = EPOLLIN;
+    ev.data.fd = sockfd;
+
+    epoll_ctl(epfd, EPOLL_CTL_ADD, sockfd, &ev);
+
+    struct epoll_event events[1024] = {0};
+    while (1) {
+        int nready = epoll_wait(epfd, events, 1024, -1);
+
+        int i = 0;
+        for (i = 0; i < nready; i++) {
+            int connfd = events[i].data.fd;
+            if (connfd == sockfd) {
+                struct sockaddr_in clientaddr;
+                socklen_t len = sizeof(clientaddr);
+                int clientfd = accept(sockfd, (struct sockaddr*)&clientaddr, &len);
+                
+                printf("socket: %d\n", clientfd);
+
+                ev.events = EPOLLIN;
+                ev.data.fd = clientfd;
+                epoll_ctl(epfd, EPOLL_CTL_ADD, clientfd, &ev);
+            } else if (events[i].events & EPOLLIN) {
+                char buffer[128] = {0};
+                int count = recv(connfd, buffer, sizeof(buffer), 0);
+                if (count == 0) {
+                    printf("disconnect\n");
+                    epoll_ctl(epfd, EPOLL_CTL_DEL, connfd, NULL);
+                    close(connfd);
+                    continue;
+                }
+                send(connfd, buffer, count, 0);
+                printf("clientfd: %d, count: %d, buffer: %s\n", connfd, count, buffer);
+            }
+        }
+    }
+
 
 #endif
     getchar();
